de7c2ab82995c25940d4001e6d571a61676bfb1b
“your <it20154226@my.sliit.lk>
2022-08-08 13:55:02 +0530

    Finished

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./App.css";
 import TodoList from "./components/TodoList";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="todo-app">
+      {/* //Helmet */}
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>My Todo</title>
+        <link rel="canonical" href="/home" />
+      </Helmet>
+      ;
       <TodoList />
     </div>
   );
